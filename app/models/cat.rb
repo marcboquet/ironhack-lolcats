6f233acdd22e0fb9056d258f2430798b8ea84925
Marc Boquet <marc@apalan.cat>
2014-10-08 17:28:27 +0200

    mount uploader to Cat
    Also, run `touch tmp/restart.txt` to get new gem loaded

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,4 +2,7 @@ class Cat < ActiveRecord::Base
   validates_presence_of :name
 
   belongs_to :owner
+
+  mount_uploader :picture, # name of the database field
+                 CatPictureUploader
 end
